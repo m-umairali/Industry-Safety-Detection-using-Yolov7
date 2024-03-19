74f5b501cab892900bb1a6562f40a92afe11136c
umair <umairalism@gmail.com>
2024-03-19 11:15:04 +0500

    folder structre added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Umair',
+    author_email= 'm.umarali8899@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
